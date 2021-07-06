395dc7578f16dbb83a9ec3db06747940395facc8
Christian L. Angelone <christian.angelone@wildlifestudios.com>
2021-07-05 23:18:23 -0300

    Removes -e from echo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ exit_on_error() {
     text=$2
     last_command=${@:2}
     if [ $exit_code -ne 0 ]; then
-        >&2 echo -e "$text."
+        >&2 echo "$text."
         exit $exit_code
     fi
 }
@@ -17,13 +17,13 @@ LATEST_RELEASE=$(curl -L -s -H 'Accept: application/json' https://github.com/chr
 LATEST_VERSION=$(echo $LATEST_RELEASE | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 BIN_URL="https://github.com/christiangelone/bang/releases/download/$LATEST_VERSION/bang_${OS}_${ARCH}"
 
-echo -e "⬇️  Downloading bang."
+echo "⬇️  Downloading bang."
 curl -fsL -H 'Accept: application/octet-stream' $BIN_URL -o bang
 exit_on_error $? "💀  Failed to download bang bootstrap" !!
 chmod +x bang
 exit_on_error $? "💀  Failed bootstrap executable" !!
 
-echo -e "⚙️  Installing bang."
+echo "⚙️  Installing bang."
 ./bang install github.com/christiangelone/bang >/dev/null 2>&1
 exit_on_error $? "💀  Failed to install bang" !!
 rm ./bang
@@ -46,10 +46,10 @@ if [[ "$PATH" != *"$BANG_PATH"* ]]; then
     ;;
   esac
 
-  echo -e "✏️  Adding bang to your PATH in $SHELL_PROFILE"
-  echo -e "    \033[0;34m•\033[0m Please run \033[1;33msource $SHELL_PROFILE\033[0m"
-  echo -e "\nBANG_PATH=$BANG_PATH" >> $SHELL_PROFILE
+  echo "✏️  Adding bang to your PATH in $SHELL_PROFILE"
+  echo "    \033[0;34m•\033[0m Please run \033[1;33msource $SHELL_PROFILE\033[0m"
+  echo "\nBANG_PATH=$BANG_PATH" >> $SHELL_PROFILE
   echo 'export PATH="$PATH:$BANG_PATH"' >> $SHELL_PROFILE
 fi
 
-echo -e "🎉 Bang installed!"
+echo "🎉 Bang installed!"
